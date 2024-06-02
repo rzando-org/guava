a52c6fa1e08ffc1a84afb10807b8d63394f80af8
Ricardo Zandonai <squall@outlook.com>
2024-06-02 15:29:31 -0300

    Initial commit

---
+++ guava-gwt/src-super/com/google/common/collect/super/com/google/common/collect/RegularImmutableList.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2009 The Guava Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.common.collect;
+
+import static java.util.Collections.emptyList;
+import static java.util.Collections.unmodifiableList;
+
+import java.util.List;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+/**
+ * GWT emulated version of {@link RegularImmutableList}.
+ *
+ * @author Hayward Chan
+ */
+@ElementTypesAreNonnullByDefault
+class RegularImmutableList<E> extends ForwardingImmutableList<E> {
+
+  static final ImmutableList<Object> EMPTY = new RegularImmutableList<Object>(emptyList());
+
+  private final List<E> delegate;
+  @Nullable E forSerialization;
+
+  RegularImmutableList(List<E> delegate) {
+    // TODO(cpovirk): avoid redundant unmodifiableList wrapping
+    this.delegate = unmodifiableList(delegate);
+  }
+
+  @Override
+  List<E> delegateList() {
+    return delegate;
+  }
+}
